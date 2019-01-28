ad4477d67584c37df93ca10fed20789e7ca13c71
Olga Tkach <tkacholya@gmail.com>
2019-01-28 12:49:43 -0500

    fixed some issues with config

---
+++ src/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SPACE_ID: 's4w6uvusl4ul',
+  ACCESS_TOKEN:
+    '020705551d6f6a4fcbbc8a2ff067e618da87709718aacd25df424648bd75a5ac'
+};
---
+++ src/config/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  SPACE_ID: process.env.SPACE_ID,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN
+};
